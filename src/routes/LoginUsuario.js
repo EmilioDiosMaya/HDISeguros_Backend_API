a084ca8183c5a301a29d88b31ea9e50d1d18d0eb
Emilio Antonio Alarcón Santos <emiliostd@outlook.com>
2022-06-16 00:05:44 -0500

    Corrección de las rutas de login

--- src/routes/LoginUsuario.js
+++ src/routes/LoginUsuario.js
@@ -2,9 +2,9 @@ const { Router } = require('express');
 const router = Router();
 const mysqlConnection = require('../database');
 
-ruta.post('/', (req, res) => {
+router.post('/', (req, res) => {
     const { Telefono, NombreUsuario, Contraseña } = req.body
-    mysqlConnection.query('CALL LoginUsuario(?, ?)', [Telefono, NombreUsuario, Contraseña], (err, rows) => {
+    mysqlConnection.query('CALL LoginUsuario(?, ?, ?)', [Telefono, NombreUsuario, Contraseña], (err, rows) => {
         if (!err) {
             if (!rows[0][0].hasOwnProperty('idUsuario')) {
                 res.status(200).json(rows[0][0])
